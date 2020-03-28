d9e984085107f831d44dd8cda33a76c416d698c1
dash-sampadananda <info,sampadananda@gmail.com>
2020-03-28 17:47:43 +0530

    adding a dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AddQuestionComponent } from './add-question/add-question.component';
+
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {RoutingModule} from './routing/routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {HomeComponent} from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddQuestionComponent
+    NavbarComponent,
+    HomeComponent
   ],
+  
   imports: [
+    /* Angular Material Modules */
     BrowserModule,
-    FormsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    RoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
